b731bfdcfcbb5b50b7ea8246f96db9499b492b8d
Mazafard <mazafard@irantalent.com>
2020-04-09 09:34:48 +0430

    :+1: add URL features

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # url-shortener
 The fastest way to reduce the length of URLs with statistics
+
+
+- [x] Register
+- [x] Login
+    - [x] Permission
+- [x] Create Short URL
+    - [x] handle custom uri
+    - [x] handle duplicate uri
+ 
+- [ ] Redirect
+- [ ] Analytics
--- service/tests.py
+++ service/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory
+from model_bakery import baker
 
-# Create your tests here.
+from service.models import Url
+
+class UrlModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+
+        cls.user = baker.make(User)
+        Url.objects.create(link='google.com', uri='', user=cls.user)
+
+
+    def test_url_label(self):
+        url = Url.objects.get(id=1)
+        field_label = url._meta.get_field('link').verbose_name
+        self.assertEquals(field_label, 'link')
+
